833f489938d1eb006f375debf1d631d53c9a2abf
Angel Huerta <angeladrianhp@hotmail.com>
2020-06-17 12:55:18 -0500

    Creamos el fetchEvents en vuex y añadimos paginación a la lista de eventos

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -10,8 +10,8 @@ const apiClient = axios.create({
 })
 
 export default {
-  getAllEvents() {
-    return apiClient.get('/events')
+  getAllEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
   },
   getEvent(id) {
     return apiClient.get('/events/' + id)
--- src/store/index.js
+++ src/store/index.js
@@ -19,11 +19,18 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: []
+    events: [],
+    eventsTotal: 0
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, totalEvents) {
+      state.eventsTotal = totalEvents
     }
   },
   actions: {
@@ -31,6 +38,19 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getAllEvents(perPage, page)
+        .then(response => {
+          commit(
+            'SET_EVENTS_TOTAL',
+            parseInt(response.headers['x-total-count'])
+          )
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log(`There was an error: ${error}`)
+        })
     }
   },
   getters: {
