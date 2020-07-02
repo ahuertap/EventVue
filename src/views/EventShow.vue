89a2f2fa0e019f49f161267b97e7c7d7c939f97b
Angel Huerta <angeladrianhp@hotmail.com>
2020-07-02 18:21:22 -0500

    Usamos in-component route gouards para utilizar nuestra barra de progreso

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -29,7 +29,9 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState } from 'vuex'
+import NProgress from 'nprogress'
+import store from '@/store/index'
 
 export default {
   name: 'EventShow',
@@ -38,19 +40,17 @@ export default {
       type: Number
     }
   },
-  created() {
-    this.fetchEvent(this.id)
+  beforeRouteEnter(to, from, next) {
+    NProgress.start()
+    store.dispatch('event/fetchEvent', to.params.id).then(() => {
+      NProgress.done()
+      next()
+    })
   },
   computed: {
     ...mapState({
       event: state => state.event.event
     })
-  },
-  methods: {
-    /**
-     * mapActions(namespace, [actions_to_map])
-     */
-    ...mapActions('event', ['fetchEvent'])
   }
 }
 </script>
