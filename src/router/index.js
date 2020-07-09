2bcdbd83ac76c60f5da508501aec5bcba65ea7a8
Angel Huerta <angeladrianhp@hotmail.com>
2020-07-09 13:52:30 -0500

    Agregamos componenten que muestra una pagina personalizada para el error 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import EventList from '@/views/EventList'
 import EventShow from '@/views/EventShow'
 import NProgress from 'nprogress'
 import store from '@/store/index'
+import NotFound from '@/views/NotFound'
 
 Vue.use(VueRouter)
 
@@ -21,16 +22,32 @@ const routes = [
     component: EventShow,
     props: true,
     beforeEnter(to, from, next) {
-      store.dispatch('event/fetchEvent', to.params.id).then(event => {
-        to.params.event = event
-        next()
-      })
+      store
+        .dispatch('event/fetchEvent', to.params.id)
+        .then(event => {
+          to.params.event = event
+          next()
+        })
+        .catch(() => {
+          next({ name: '404', params: { resource: 'event' } })
+        })
     }
   },
   {
     path: '/event/create',
     name: 'event-create',
     component: EventCreate
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: NotFound,
+    props: true
+  },
+  {
+    // Toda ruta que no exista se redireccionara al componente 404
+    path: '*',
+    redirect: { name: '404', params: { resource: 'page' } }
   }
 ]
 
